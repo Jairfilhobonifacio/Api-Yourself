355f7f5106816b7704619136b2a9bfe8a8240198
Jair Filho <jbonifaciomoreirafilho@gmail.com>
2025-07-22 01:32:56 -0300

    update vercel

--- frontend_new/src/components/index.ts
+++ frontend_new/src/components/index.ts
@@ -9,7 +9,7 @@ export * from './ui/custom-toast';
 
 // Componentes que não foram substituídos
 export * from './ui/radio-group';
-export * from './ui/select';
+// export * from './ui/select';  // Removido pois não está sendo usado
 export * from './ui/switch';
 export * from './ui/tabs';
  
